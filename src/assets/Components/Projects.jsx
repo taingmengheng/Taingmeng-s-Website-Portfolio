5b57b045ec3397592ff7fe4c4b9adc0165dbdd1e
H.Taingmeng <hengtaingmeng@gmail.com>
2024-04-01 16:00:04 +0700

    Implement: new section to the landing page

---
+++ src/assets/Components/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className="pt-24">
+      <div className="w-full text-center mb-12">
+        <p className="text-5xl font-bold ">Projects</p>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
